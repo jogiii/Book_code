f362c20113668477524b377bdc8b9c0a35703f12
jogi <joginder.kumar1991@gmail.com>
2023-11-19 10:15:42 +0530

    Create Page.java

---
+++ Page.java
@@ -0,0 +1,19 @@
+package com.bigquery.demo;
+
+public class Page {
+    private int pageNo;
+    private String pageContent;
+
+    public Page(int pageNo, String pageContent) {
+        this.pageNo = pageNo;
+        this.pageContent = pageContent;
+    }
+
+    public int getPageNo() {
+        return pageNo;
+    }
+
+    public String getPageContent() {
+        return pageContent;
+    }
+}
